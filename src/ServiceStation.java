12de94831f30f74eba52ddf4d04d99a76f469f16
Олег <olegcelysev778@gmail.com>
2023-08-07 13:07:55 +0300

    Домашка 16

---
+++ src/ServiceStation.java
@@ -0,0 +1,11 @@
+public class ServiceStation {
+    public void check(Servicable servicable) {
+        servicable.service();
+    }
+    public void checkAll(Servicable[] servicables) {
+        for (Servicable servicable : servicables)
+            servicable.service();
+    }
+}
+
+
---
+++ src/Transport.java
@@ -0,0 +1,22 @@
+public class Transport implements  Servicable{
+    private final String modelName;
+    private final int wheelsCount;
+    public Transport(String modelName, int wheelsCount) {
+        this.modelName = modelName;
+        this.wheelsCount = wheelsCount;
+    }
+    public String getModelName() {
+        return modelName;
+    }
+    public int getWheelsCount() {
+        return wheelsCount;
+    }public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+    @Override
+    public void service() {
+        for (int i = 0; i < wheelsCount; i++) {
+            updateTyre();
+        }
+    }
+}
